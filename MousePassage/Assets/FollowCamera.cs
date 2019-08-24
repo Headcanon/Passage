c3d9bcf3a099085e5eec51ced934c5315ec244fd
lucas <bouggroug@hotmail.com>
2019-08-24 15:44:51 -0300

    Movimento e camera

---
+++ MousePassage/Assets/FollowCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    Transform alvo;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        alvo = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = alvo.position + offset;
+    }
+}
---
+++ MousePassage/Assets/RatMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatMovement : MonoBehaviour
+{
+    public float vel;
+    public float moveVel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector2(Input.GetAxis("Horizontal") * moveVel, vel) * Time.deltaTime);
+    }
+}
