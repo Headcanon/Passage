a4a43569cf75b3f8d39fd489a2a1de5f69e4bd69
lucas <bouggroug@hotmail.com>
2019-08-24 23:07:30 -0300

    Sei lá esses negócios de git, amanhã eu resolvo isso

---
+++ MousePassage/Assets/EnemyMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float slowVel;
+
+    float vel;
+
+    Transform alvo;
+    RatMovement alvoScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        alvo = GameObject.FindGameObjectWithTag("Player").transform;
+        alvoScript = alvo.GetComponent<RatMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, alvo.position) > 12)
+        {
+            vel = alvoScript.vel;
+        }
+        else
+        {
+            vel = slowVel;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(new Vector2(0, vel) * Time.deltaTime);
+    }
+}
---
+++ MousePassage/Assets/TileManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    
+    public List<GameObject> tiles;
+    List<GameObject> activeTiles;
+
+    Transform alvo;
+
+    float spawnY = 0;
+    float tileLeght = 18;
+    float tilesOnScreen = 5;
+
+    int lastPrefabIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeTiles = new List<GameObject>();
+        alvo = GameObject.FindGameObjectWithTag("Player").transform;
+
+        lastPrefabIndex = Random.Range(0, activeTiles.Count);
+
+        for (int i = 0; i < tilesOnScreen; i++)
+        {
+            SpawnTile();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(alvo.position.y - tileLeght> (spawnY - tilesOnScreen * tileLeght))
+        {
+            SpawnTile();
+            DestroyTile();
+        }
+    }
+
+    void SpawnTile()
+    {
+        GameObject go;
+        go = Instantiate(tiles[RandomIndex()], transform) as GameObject;
+        go.transform.position = Vector2.up * spawnY;
+
+        spawnY += tileLeght;
+
+        activeTiles.Add(go);
+    }
+
+    void DestroyTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+
+    int RandomIndex()
+    {
+        if (tiles.Count <= 1)
+            return 0;
+
+        int newIndex = lastPrefabIndex;
+        while (newIndex == lastPrefabIndex)
+        {
+            newIndex = Random.Range(0, tiles.Count);
+        }
+        lastPrefabIndex = newIndex;
+        return newIndex;
+    }
+}
